a7bb138467e3979c3b05b2e98fb1d75c2c36a41a
lizz <1335206475@qq.com>
2021-08-22 13:44:19 +0800

    refactor: change module

--- src/main/java/com/bigboss/controller/MealsController.java
+++ src/main/java/com/bigboss/controller/MealsController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-08-04
  */
 @RestController
-@RequestMapping("/blog/meals")
+@RequestMapping("/meals")
 public class MealsController {
 
 }
