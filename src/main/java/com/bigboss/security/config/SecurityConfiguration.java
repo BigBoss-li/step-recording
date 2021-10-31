f177a18aa062d8f1853319f7b13eed39b0b186cd
lizz <1335206475@qq.com>
2021-10-31 09:42:15 +0800

    refactor: springSecurity 通过配置文件开启关闭

--- src/main/java/com/bigboss/security/config/SecurityConfiguration.java
+++ src/main/java/com/bigboss/security/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.bigboss.security.config;
 import com.bigboss.handler.LoginFailureHandler;
 import com.bigboss.handler.LoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,9 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value(value = "${security.enabled}")
+    private Boolean enabled;
+
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -25,7 +29,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
-        http.csrf().disable().formLogin().loginProcessingUrl("/auth/token/by-code").usernameParameter("code").passwordParameter("code").successHandler(new LoginSuccessHandler()).failureHandler(new LoginFailureHandler());
+        if (enabled) {
+            http.authorizeRequests().anyRequest().authenticated();
+        } else {
+            http.authorizeRequests().anyRequest().permitAll();
+        }
+        http.csrf().disable()
+                .formLogin().loginProcessingUrl("/auth/token/by-code").usernameParameter("code").passwordParameter("code")
+                .successHandler(new LoginSuccessHandler()).failureHandler(new LoginFailureHandler());
     }
 }
